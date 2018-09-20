393d69e0ea5ad1e86fc0d0b18b688d296c985875
Roberto M. Oliva <floyd303@gmail.com>
2018-09-20 19:50:43 +0200

    Removed no longer needed custom typings

--- custom-typings/require-dir.d.ts
+++
@@ -1,8 +0,0 @@
-declare let requireDir: (directory: string, options?: any)=> { [path: string]: any };
-declare module 'require-dir' {
-  // Type definitions for require-dir 0.3
-  // Project: https://github.com/aseemk/requireDir
-  // Definitions by: weekens <https://github.com/weekens>
-  // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-  export = requireDir;
-}

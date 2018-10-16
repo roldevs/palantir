b20e17d8ddc1c117a1fbba7c338f5fdb1d93f8e8
Roberto M. Oliva <floyd303@gmail.com>
2018-10-17 00:14:59 +0200

    Improved UI

--- src/client/scaleapp/modules/results/view.ts
+++ src/client/scaleapp/modules/results/view.ts
@@ -3,22 +3,10 @@ import {h} from 'snabbdom';
 import {VNode} from 'snabbdom/vnode';
 import {
   IElementDefinition,
-  IRelatedElement,
-  IRelatedElements,
-  IElement,
 } from '../../../../lib/typings';
 import { JSONprettify } from '../../../../lib/utils';
 import elementPanel from '../../../ui/element_panel';
 import { IResultsController, IResultsState } from './controller';
-import { IResultsModel } from './model';
-
-const parsedResult: (model: Array<IElementDefinition | null>) => string =
-(model) => {
-  if (!model) {
-    return 'No se puede obtener un valor';
-  }
-  return JSONprettify(model);
-};
 
 const viewFn: (model: IResultsState, controller: IResultsController) => VNode =
 (model) => {
